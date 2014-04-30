39fb180a8addac37a5bcbc408fc9d7c064fda263
Bart Wood <bartwood@gmail.com>
2014-04-30 14:48:56 -0600

    Hooked up email

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var exec = require('child_process').exec;
 var fs = require('fs');
 var dateFormat = require('dateformat');
+var nodemailer = require('nodemailer');
 
 var port = 3000;
 var releaseMode = process.argv[2] || 'PROD';
@@ -40,9 +41,16 @@ app.get('/', function(req, res){
         deployClient();
     }
 
-    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.writeHeader(200, {'Content-Type': 'text/html'});
     res.write(homeHtml);
     res.end();
+});
+
+var smtpTransport = nodemailer.createTransport('Gmail', {
+    auth: {
+        user: 'rich@wifunds.com',
+        pass: 'mypass12'
+    }
 });
 
 // handle emails
@@ -50,17 +58,45 @@ app.post('/sendEmail', function(req, res){
     try {
         // write the email to a file as a backup just in case
         var fileName = 'emails/' + dateFormat(new Date(), 'yyyy-mm-dd-h_MM_ss') + '.txt';
-        var backup = 'Email: ' + req.body.fromAddress + '\r\nMessage: ' + req.body.message;
+        var backup = 'Email: ' + req.body.fromAddress + '\r\n\r\nMessage: ' + req.body.message;
         fs.writeFile(fileName, backup, function (err) {
             if (err) throw err;
         });
     }
     catch(e) {
-        console.error('!!!!CRITICAL ERROR, could not save an email to a file!!! ', req && req.body && req.body.fromAddress, req && req.body && req.body.message);
+        try {
+            console.error('!!!!CRITICAL ERROR, could not save an email to a file!!! ', req && req.body && req.body.fromAddress, req && req.body && req.body.message);
+        }
+        catch(e2){
+            // no idea how that could happen but I'm being safe
+        }
     }
 
     // Actually send the email now
     console.log('sending email: ', req.body.fromAddress, req.body.message);
+
+    try {
+
+        var mailOptions = {
+            from: 'Trade OMS Sales <sales@tradeoms.com>',
+            to: 'rich@wifunds.com, bartwood@gmail.com', // list of receivers
+            subject: 'Important! Inquiry from tradeoms.com',
+            text: 'From: ' + req.body.fromAddress + '\r\nMessage: ' + req.body.message
+        };
+
+        smtpTransport.sendMail(mailOptions, function (error, response) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                console.log('Message sent: ' + response.message);
+            }
+        });
+    }
+    catch(e){
+        
+    }
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('{}');
     res.end();
